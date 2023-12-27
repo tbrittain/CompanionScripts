7503a50d7a6abe16a7385c578594994df9bbca5c
Trey Brittain <treybrittain@gmail.com>
2023-12-26 23:16:10 -0600

    Fix top awardees

--- top_awardees.sql
+++ top_awardees.sql
@@ -1,9 +1,12 @@
 SELECT p.Id,
        P.FirstName || ' ' || P.LastName AS PlayerName,
-       COUNT(PTH.Id)                    AS NumberOfTeams
+       COUNT(PA.Id)                     AS NumAwards,
+       COUNT(PA.Id) FILTER (WHERE PA.Importance IN (0, 1)) AS NumMajorAwards,
+         COUNT(PA.Id) FILTER (WHERE PA.OriginalName = 'All-Star') AS NumAllStarAwards,
+       JSON_GROUP_ARRAY(PA.OriginalName)        AS Awards
 FROM PlayerSeasons PS
          JOIN main.Players P on P.Id = PS.PlayerId
-         JOIN PlayerTeamHistory PTH on PTH.PlayerSeasonId = PS.Id
-WHERE PTH.SeasonTeamHistoryId IS NOT NULL
+         JOIN main.PlayerAwardPlayerSeason PAPS on PS.Id = PAPS.PlayerSeasonsId
+         JOIN main.PlayerAwards PA on PAPS.AwardsId = PA.Id
 GROUP BY p.Id, PlayerName
-ORDER BY NumberOfTeams DESC;
+ORDER BY NumAwards DESC;
