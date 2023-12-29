a364c6c1cffc28a0b0526fa2bc4f71e21d38c2b2
Trey Brittain <treybrittain@gmail.com>
2023-12-28 20:09:39 -0600

    Include pitchers in most_improved_players_year_to_year.sql

--- most_improved_players_year_to_year.sql
+++ most_improved_players_year_to_year.sql
@@ -1,9 +1,10 @@
 WITH PlayerStats AS (SELECT PSGS.PlayerSeasonId,
-                            PSGS.Power + PSGS.Speed + PSGS.Contact + PSGS.Arm + PSGS.Fielding AS TotalStats
+                            PSGS.Power + PSGS.Speed + PSGS.Contact +
+                            COALESCE(PSGS.Arm, 0) + PSGS.Fielding +
+                            COALESCE(PSGS.Velocity, 0) +
+                            COALESCE(PSGS.Junk, 0) + COALESCE(PSGS.Accuracy, 0) AS TotalStats
                      FROM PlayerSeasonGameStats PSGS
-                              JOIN main.PlayerSeasons PS on PS.Id = PSGS.PlayerSeasonId
-                              JOIN main.Players P on P.Id = PS.PlayerId
-                     WHERE P.PitcherRoleId IS NULL),
+                              JOIN main.PlayerSeasons PS on PS.Id = PSGS.PlayerSeasonId),
      SeasonStats AS (SELECT P.FirstName || ' ' || P.LastName AS PlayerName,
                             S.Number                         AS SeasonNumber,
                             PlayerStats.TotalStats,
@@ -16,8 +17,10 @@ SELECT SS1.PlayerName,
        SS1.SeasonNumber,
        CASE
            WHEN SS2.TotalStats IS NOT NULL THEN SS1.TotalStats - SS2.TotalStats
-           END AS DifferenceFromPrevSeason
+           END AS DifferenceFromPrevSeason,
+       P2.PitcherRoleId
 FROM SeasonStats SS1
+         JOIN Players P2 ON SS1.PlayerId = P2.Id
          LEFT JOIN SeasonStats SS2 ON SS1.PlayerId = SS2.PlayerId AND SS1.SeasonNumber = SS2.SeasonNumber + 1
 WHERE SS2.TotalStats IS NOT NULL
 ORDER BY DifferenceFromPrevSeason DESC
