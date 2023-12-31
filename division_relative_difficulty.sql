e4f7d4d15e0c667e290754f9c2e26726a5301dfa
Trey Brittain <treybrittain@gmail.com>
2023-12-31 01:02:10 -0600

    Wrap in CTE

--- division_relative_difficulty.sql
+++ division_relative_difficulty.sql
@@ -1,17 +1,20 @@
-SELECT D.Name,
-       S.Number                                                                                    as SeasonNumber,
-       SUM(STH.Wins)                                                                               as Wins,
-       SUM(STH.Losses)                                                                             as Losses,
-       AVG(STH.Wins)                                                                               as AverageWins,
-       SUM(STH.ExpectedWins)                                                                       as ExpectedWins,
-       SUM(STH.ExpectedLosses)                                                                     as ExpectedLosses,
-       AVG(STH.ExpectedWins)                                                                       as AverageExpectedWins,
-       RANK() OVER (PARTITION BY STH.SeasonId ORDER BY SUM(STH.Wins) DESC, SUM(STH.Losses))        as SeasonRank,
-       RANK() OVER (ORDER BY SUM(STH.Wins) DESC, SUM(STH.Losses))                                  as GlobalRank,
-       JSON_GROUP_ARRAY(JSON_OBJECT('TeamName', TNH.Name, 'Wins', STH.Wins, 'Losses', STH.Losses)) as Teams
-FROM Divisions D
-         JOIN main.SeasonTeamHistory STH on D.Id = STH.DivisionId
-         JOIN main.TeamNameHistory TNH on STH.TeamNameHistoryId = TNH.Id
-         JOIN main.Seasons S on STH.SeasonId = S.Id
-GROUP BY D.Name, S.Number
+WITH DivisionRankings AS (SELECT D.Name,
+                                 S.Number                                                                             as SeasonNumber,
+                                 SUM(STH.Wins)                                                                        as Wins,
+                                 SUM(STH.Losses)                                                                      as Losses,
+                                 AVG(STH.Wins)                                                                        as AverageWins,
+                                 SUM(STH.ExpectedWins)                                                                as ExpectedWins,
+                                 SUM(STH.ExpectedLosses)                                                              as ExpectedLosses,
+                                 AVG(STH.ExpectedWins)                                                                as AverageExpectedWins,
+                                 RANK() OVER (PARTITION BY STH.SeasonId ORDER BY SUM(STH.Wins) DESC, SUM(STH.Losses)) as SeasonRank,
+                                 RANK() OVER (ORDER BY SUM(STH.Wins) DESC, SUM(STH.Losses))                           as GlobalRank,
+                                 JSON_GROUP_ARRAY(JSON_OBJECT('TeamName', TNH.Name, 'Wins', STH.Wins, 'Losses',
+                                                              STH.Losses))                                            as Teams
+                          FROM Divisions D
+                                   JOIN main.SeasonTeamHistory STH on D.Id = STH.DivisionId
+                                   JOIN main.TeamNameHistory TNH on STH.TeamNameHistoryId = TNH.Id
+                                   JOIN main.Seasons S on STH.SeasonId = S.Id
+                          GROUP BY D.Name, S.Number)
+SELECT *
+FROM DivisionRankings
 ORDER BY Wins DESC
