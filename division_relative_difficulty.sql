5b71078d14e85439d95cfc7597e52f1a5786198f
Trey Brittain <treybrittain@gmail.com>
2023-12-30 20:23:49 -0600

    Add division_relative_difficulty.sql

---
+++ division_relative_difficulty.sql
@@ -0,0 +1,14 @@
+SELECT D.Name,
+       S.Number                                                                                    as SeasonNumber,
+       SUM(STH.Wins)                                                                               as Wins,
+       SUM(STH.Losses)                                                                             as Losses,
+       AVG(STH.Wins)                                                                               as AverageWins,
+       RANK() OVER (PARTITION BY STH.SeasonId ORDER BY SUM(STH.Wins) DESC, SUM(STH.Losses))        as SeasonRank,
+       RANK() OVER (ORDER BY SUM(STH.Wins) DESC, SUM(STH.Losses))                                  as GlobalRank,
+       JSON_GROUP_ARRAY(JSON_OBJECT('TeamName', TNH.Name, 'Wins', STH.Wins, 'Losses', STH.Losses)) as Teams
+FROM Divisions D
+         JOIN main.SeasonTeamHistory STH on D.Id = STH.DivisionId
+         JOIN main.TeamNameHistory TNH on STH.TeamNameHistoryId = TNH.Id
+         JOIN main.Seasons S on STH.SeasonId = S.Id
+GROUP BY D.Name, S.Number
+ORDER BY Wins DESC
