3ae75f61dcf73631807471107cddf69a5bd7454b
Trey Brittain <treybrittain@gmail.com>
2024-01-01 19:20:40 -0600

    Add HomeRunToRbiRatio to most_hr_fewest_hits_seasons.sql

--- most_hr_fewest_hits_seasons.sql
+++ most_hr_fewest_hits_seasons.sql
@@ -1,17 +1,19 @@
 SELECT p.Id,
-       P.FirstName || ' ' || P.LastName AS PlayerName,
-       S.Number                         AS SeasonNumber,
+       P.FirstName || ' ' || P.LastName                               AS PlayerName,
+       S.Number                                                       AS SeasonNumber,
        PSBS.AtBats,
        PSBS.Hits,
        PSBS.BattingAverage,
        PSBS.HomeRuns,
-       PSBS.HomeRuns / CAST(PSBS.Hits AS FLOAT)        AS HomeRunToHitRatio,
-         PSBS.HomeRuns / CAST(PSBS.Hits AS FLOAT) / PSBS.BattingAverage AS HighHrLowAvgRatio
+       PSBS.RunsBattedIn,
+       PSBS.HomeRuns / CAST(PSBS.Hits AS FLOAT)                       AS HomeRunToHitRatio,
+       PSBS.HomeRuns / CAST(PSBS.Hits AS FLOAT) / PSBS.BattingAverage AS HighHrLowAvgRatio,
+       PSBS.HomeRuns / CAST(PSBS.RunsBattedIn AS FLOAT)               AS HomeRunToRbiRatio
 FROM PlayerSeasonBattingStats PSBS
          JOIN main.PlayerSeasons PS on PS.Id = PSBS.PlayerSeasonId
          JOIN main.Players P on PS.PlayerId = P.Id
          JOIN Seasons S on PS.SeasonId = S.Id
          JOIN main.PlayerSeasonGameStats PSGS ON PS.Id = PSGS.PlayerSeasonId
 WHERE PSBS.IsRegularSeason = 1
-AND PSBS.PlateAppearances > 3.1 * 162 -- qualified for 162 game season
+  AND PSBS.PlateAppearances > 3.1 * 162 -- qualified for 162 game season
 ORDER BY HighHrLowAvgRatio DESC
