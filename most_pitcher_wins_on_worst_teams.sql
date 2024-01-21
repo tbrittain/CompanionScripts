904d7113e5d520880c7a63ef777a69f3cc1ace10
Trey Brittain <treybrittain@gmail.com>
2024-01-21 09:08:41 -0600

    Add most_pitcher_wins_on_worst_teams.sql

---
+++ most_pitcher_wins_on_worst_teams.sql
@@ -0,0 +1,20 @@
+SELECT P.FirstName || ' ' || P.LastName            AS PlayerName,
+       S.Number                                    AS SeasonNumber,
+       PSPS.Wins,
+       PSPS.Losses,
+       PSPS.WinPercentage,
+       PSPS.EarnedRunAverage,
+       PSPS.EraMinus,
+       TNH.Name                                    AS TeamName,
+       STH.Wins                                    AS TeamWins,
+       PSPS.Wins * (1 / CAST(STH.WinPercentage AS FLOAT)) AS WeightedWinsPerTeamLoss
+FROM PlayerSeasonPitchingStats PSPS
+         JOIN main.PlayerSeasons PS ON PS.Id = PSPS.PlayerSeasonId
+         JOIN main.Seasons S ON S.Id = PS.SeasonId
+         JOIN main.Players P ON P.Id = PS.PlayerId
+         JOIN main.PlayerTeamHistory PTH ON PTH.PlayerSeasonId = PS.Id
+         JOIN main.SeasonTeamHistory STH ON PTH.SeasonTeamHistoryId = STH.Id
+         JOIN main.Teams T ON STH.TeamId = T.Id
+         JOIN main.TeamNameHistory TNH ON STH.TeamNameHistoryId = TNH.Id
+WHERE PSPS.IsRegularSeason = 1
+ORDER BY WeightedWinsPerTeamLoss DESC
