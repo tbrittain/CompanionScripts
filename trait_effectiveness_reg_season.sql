054e44ac4db6a60664b5b4f4dee9cab0792b9138
Trey Brittain <treybrittain@gmail.com>
2024-02-11 15:53:31 -0600

    Finalize trait_effectiveness_reg_season.sql

--- trait_effectiveness_reg_season.sql
+++ trait_effectiveness_reg_season.sql
@@ -7,8 +7,7 @@ WITH AllTraits AS (SELECT T.Id, T.Name
                                    JOIN main.PlayerSeasonTrait PST ON PS.Id = PST.PlayerSeasonsId
                                    JOIN AllTraits T ON PST.TraitsId = T.Id
                                    JOIN main.Players P ON P.Id = PS.PlayerId
-                          WHERE P.PitcherRoleId IS NULL
-                            AND PSBS.IsRegularSeason = 1),
+                          WHERE PSBS.IsRegularSeason = 1),
      SeasonsWithoutTrait AS (SELECT *
                              FROM main.PlayerSeasonBattingStats PSBS
                                       JOIN main.PlayerSeasons PS ON PS.Id = PSBS.PlayerSeasonId
@@ -16,27 +15,32 @@ WITH AllTraits AS (SELECT T.Id, T.Name
                              WHERE PS.Id NOT IN (SELECT PlayerSeasonsId
                                                  FROM main.PlayerSeasonTrait PST
                                                           JOIN AllTraits T ON PST.TraitsId = T.Id)
-                               AND PSBS.IsRegularSeason = 1
-                               AND P.PitcherRoleId IS NULL)
-SELECT TraitName,
-       Type,
-       AVG(AvgOpsPlus) AS AvgOpsPlus,
-       SUM(NumSeasons) AS NumSeasons,
-       AVG(AvgAtBats)  AS AvgAtBats
-FROM (SELECT TraitName,
-             'With Trait'                     AS Type,
-             AVG(SeasonsWithTrait.OpsPlus)    AS AvgOpsPlus,
-             COUNT(SeasonsWithTrait.PlayerId) AS NumSeasons,
-             AVG(SeasonsWithTrait.AtBats)     AS AvgAtBats
-      FROM SeasonsWithTrait
-      GROUP BY TraitName
-      UNION ALL
-      SELECT NULL                                AS TraitName,
-             'Without Trait'                     AS Type,
-             AVG(SeasonsWithoutTrait.OpsPlus)    AS AvgOpsPlus,
-             COUNT(SeasonsWithoutTrait.PlayerId) AS NumSeasons,
-             AVG(SeasonsWithoutTrait.AtBats)     AS AvgAtBats
-      FROM SeasonsWithoutTrait
-      GROUP BY TraitName) AS CombinedResults
-GROUP BY TraitName, Type
-ORDER BY AvgOpsPlus DESC;
+                               AND PSBS.IsRegularSeason = 1),
+     Results AS (SELECT TraitName,
+                        Type,
+                        AVG(AvgOpsPlus) AS AvgOpsPlus,
+                        SUM(NumSeasons) AS NumSeasons,
+                        AVG(AvgAtBats)  AS AvgAtBats
+                 FROM (SELECT TraitName,
+                              'With Trait'                     AS Type,
+                              AVG(SeasonsWithTrait.OpsPlus)    AS AvgOpsPlus,
+                              COUNT(SeasonsWithTrait.PlayerId) AS NumSeasons,
+                              AVG(SeasonsWithTrait.AtBats)     AS AvgAtBats
+                       FROM SeasonsWithTrait
+                       GROUP BY TraitName
+                       UNION ALL
+                       SELECT NULL                                AS TraitName,
+                              'Without Trait'                     AS Type,
+                              AVG(SeasonsWithoutTrait.OpsPlus)    AS AvgOpsPlus,
+                              COUNT(SeasonsWithoutTrait.PlayerId) AS NumSeasons,
+                              AVG(SeasonsWithoutTrait.AtBats)     AS AvgAtBats
+                       FROM SeasonsWithoutTrait
+                       GROUP BY TraitName) AS CombinedResults
+                 GROUP BY TraitName, Type)
+SELECT *,
+       AvgOpsPlus - (SELECT AvgOpsPlus
+                     FROM Results
+                     WHERE Type = 'Without Trait') AS OpsPlusDiffFromNoTrait,
+       RANK() OVER (ORDER BY NumSeasons DESC)      AS TraitFrequencyRank
+FROM Results
+ORDER BY OpsPlusDiffFromNoTrait DESC
