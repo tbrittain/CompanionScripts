6ab7b6d0ae87f077563124066281919ca9fd50d3
Trey Brittain <treybrittain@gmail.com>
2024-02-15 13:05:06 -0600

    Fix % on ChampionshipPercentage

--- playoff_outcomes_by_seed.sql
+++ playoff_outcomes_by_seed.sql
@@ -1,10 +1,10 @@
 SELECT STH.PlayoffSeed,
-       SUM(CASE WHEN CW.Id IS NOT NULL THEN 1 ELSE 0 END)                           AS Championships,
-       SUM(CASE WHEN CW.Id IS NOT NULL THEN 1 ELSE 0 END) / CAST(COUNT(*) AS FLOAT) AS ChampionshipPercentage,
-       SUM(STH.PlayoffWins)                                                         AS PlayoffWins,
-       SUM(STH.PlayoffLosses)                                                       AS PlayoffLosses,
-       SUM(STH.PlayoffRunsScored)                                                   AS PlayoffRunsScored,
-       SUM(STH.PlayoffRunsAllowed)                                                  AS PlayoffRunsAllowed
+       SUM(CASE WHEN CW.Id IS NOT NULL THEN 1 ELSE 0 END)                               AS Championships,
+       SUM(CASE WHEN CW.Id IS NOT NULL THEN 1 ELSE 0 END) / CAST(COUNT(*) / 2 AS FLOAT) AS ChampionshipPercentage,
+       SUM(STH.PlayoffWins)                                                             AS PlayoffWins,
+       SUM(STH.PlayoffLosses)                                                           AS PlayoffLosses,
+       SUM(STH.PlayoffRunsScored)                                                       AS PlayoffRunsScored,
+       SUM(STH.PlayoffRunsAllowed)                                                      AS PlayoffRunsAllowed
 FROM SeasonTeamHistory STH
          LEFT JOIN main.ChampionshipWinners CW ON STH.Id = CW.SeasonTeamHistoryId
 WHERE STH.PlayoffSeed IS NOT NULL
